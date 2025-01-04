5176e042111ee06e0cb2c94694d43c72cd7dc681
slyofzero <sly.of.zero@gmail.com>
2025-01-04 09:36:42 +0530

    Fixed build errors

--- script/src/types/index.ts
+++ script/src/types/index.ts
@@ -1,3 +1,2 @@
-export * from "./web3";
 export * from "./pair";
 export * from "./tx";
--- script/src/types/user.ts
+++
@@ -1,9 +0,0 @@
-import { Timestamp } from "firebase-admin/firestore";
-
-export interface StoredUser {
-  id?: string;
-  username: string;
-  mainWallet: string;
-  wallets: string[];
-  joinedOn: Timestamp;
-}
