72e44844196f505044b07cf1c2031420842c9dd9
slyofzero <sly.of.zero@gmail.com>
2025-01-07 17:42:56 +0530

    mobile fixes

--- frontend/components/Scanning.tsx
+++ frontend/components/Scanning.tsx
@@ -1,7 +1,13 @@
 export function Scanning() {
   return (
     <div className="h-full w-full col-span-8">
-      <video className="h-full w-full object-cover" autoPlay loop>
+      <video
+        className="h-full w-full object-cover"
+        autoPlay
+        loop
+        playsInline
+        muted
+      >
         <source src="/scanning.mp4" type="video/mp4" />
       </video>
     </div>
