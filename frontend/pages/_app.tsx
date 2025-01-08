c933f744a9910a716d60a78a3c00da3d6a0abcc7
slyofzero <sly.of.zero@gmail.com>
2025-01-08 19:11:47 +0530

    favicon and title

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,18 @@
 import { AppProps } from "next/app";
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Hope-Tech</title>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
